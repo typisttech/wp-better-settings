1ba3b497d732732d60c74b014174d6264c4381d5
Tang Rufus <tangrufus@gmail.com>
2017-02-20 11:49:58 +0800

    Add return doc tag to ViewInterface::echo_kses

--- src/wp-better-settings/interface-view-interface.php
+++ src/wp-better-settings/interface-view-interface.php
@@ -44,6 +44,8 @@ interface ViewInterface {
 	 * @since 0.2.0
 	 *
 	 * @param mixed $context Context ArrayObject for which to render the view.
+	 *
+	 * @return void
 	 */
 	public function echo_kses( $context );
 }

282732593043df74523df37026d51ae7ce6b4d03
Tang Rufus <tangrufus@gmail.com>
2017-02-21 17:29:31 +0800

    Optimize input field

--- tests/acceptance/InputFieldCept.php
+++ tests/acceptance/InputFieldCept.php
@@ -1,23 +1,38 @@
 <?php
-$I = new AcceptanceTester($scenario);
-$I->wantToTest('input fields');
+$I = new AcceptanceTester( $scenario );
+$I->wantToTest( 'input fields' );
 
-$I->loginAsAdmin();
-$I->waitForText('Dashboard', 10, 'h1');
-$I->waitForElementVisible("a[href$='admin.php?page=wpbs_1']", 10); // secs
+$I->amOnWPBSOptionPage();
 
-$I->click('WP Better Settings');
-$I->click('WP Better Settings');
-$I->waitForText('WP Better Settings', 10, 'h1');
+$I->wantToTest( 'four input fields exist' );
+$I->seeNumberOfElements( 'input', 5 ); // Submit button is also a input.
 
-$I->wantToTest('input field types');
-$I->seeElement('input', [ 'type' => 'text' ]);
-$I->seeElement('input', [ 'type' => 'email' ]);
-$I->seeElement('input', [ 'type' => 'url' ]);
+$I->wantToTest( 'input field type attribute' );
+$I->seeElement( 'input', [ 'type' => 'text' ] );
+$I->seeElement( 'input', [ 'type' => 'email' ] );
+$I->seeElement( 'input', [ 'type' => 'url' ] );
 
-$I->wantToTest('input field attributes');
-$inputAttributes = $I->grabMultiple('input', 'disabled');
-$I->assertContains(true, $inputAttributes);
+$I->wantToTest( 'input field name attribute' );
+$actualNames = $I->grabMultiple( 'input', 'name' );
+$I->assertContains( 'wpbs_option_1[my_name]', $actualNames );
+$I->assertContains( 'wpbs_option_1[my_email]', $actualNames );
+$I->assertContains( 'wpbs_option_1[my_url]', $actualNames );
+$I->assertContains( 'wpbs_option_1[my_disabled_input]', $actualNames );
 
-$I->wantToTest('input field description paragraph');
-$I->see('I am a description paragraph', [ 'css' => 'p.description' ]);
+$I->wantToTest( 'input field id attribute' );
+$actualIds = $I->grabMultiple( 'input', 'id' );
+$I->assertContains( 'my_name', $actualIds );
+$I->assertContains( 'my_email', $actualIds );
+$I->assertContains( 'my_url', $actualIds );
+$I->assertContains( 'my_disabled_input', $actualIds );
+
+$I->wantToTest( 'input field aria-describedby attribute' );
+$actualAriaDescribedby = $I->grabMultiple( 'input', 'aria-describedby' );
+$I->assertContains( 'my_disabled_input-description', $actualAriaDescribedby );
+
+$I->wantToTest( 'input field disabled attribute' );
+$actualDisabled = $I->grabMultiple( 'input', 'disabled' );
+$I->assertContains( true, $actualDisabled );
+
+$I->wantToTest( 'input field description paragraph' );
+$I->see( 'I am a description paragraph', [ 'css' => 'p.description' ] );

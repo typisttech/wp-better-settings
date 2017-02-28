5b7cd1747b95c2681d7b766904eb21bdf67b82d0
Tang Rufus <tangrufus@gmail.com>
2017-03-01 01:27:08 +0800

    Optimize scripts for VVV and phpstorm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,92 @@
-module.exports = function (grunt) {
+/**
+ * WP Better Settings
+ *
+ * A simplified OOP implementation of the WP Settings API.
+ *
+ * @package   WPBS\WP_Better_Settings
+ * @author    Typist Tech <wp-better-settings@typist.tech>
+ * @license   GPL-2.0+
+ * @link      https://www.typist.tech/
+ * @copyright 2017 Typist Tech
+ */
 
-    'use strict';
+module.exports = function ( grunt ) {
 
-    // Project configuration
-    grunt.initConfig({
+	'use strict';
 
-        pkg: grunt.file.readJSON('package.json'),
+	// Project configuration.
+	grunt.initConfig( {
 
-        // Bump version numbers
-        version: {
-            composer: {
-                options: {
-                    prefix: '"version"\\:\\s+"'
-                },
-                src: ['composer.json']
-            },
-            changelog: {
-                options: {
-                    prefix: 'future-release='
-                },
-                src: ['.github_changelog_generator']
-            },
-            php: {
-                options: {
-                    prefix: '\\* Version:\\s+'
-                },
-                src: ['<%= pkg.name %>.php']
-            }
-        },
+		pkg: grunt.file.readJSON( 'package.json' ),
 
-        // Clean the build folder
-        clean: {
-            "pre-build": {
-                src: [
-                    'build/',
-                    'release/',
-                    'vendor/'
-                ]
-            }
-        },
+		// Bump version numbers.
+		version: {
+			composer: {
+				options: {
+					prefix: '"version"\\:\\s+"'
+				},
+				src: ['composer.json']
+			},
+			changelog: {
+				options: {
+					prefix: 'future-release='
+				},
+				src: ['.github_changelog_generator']
+			},
+			php: {
+				options: {
+					prefix: '\\* Version:\\s+'
+				},
+				src: ['<%= pkg.name %>.php']
+			}
+		},
 
-        // Copy to build folder
-        copy: {
-            build: {
-                expand: true,
-                src: [
-                    'partials/**',
-                    'src/**',
-                    'vendor/**',
-                    'class-plugin.php',
-                    'LICENSE',
-                    '<%= pkg.name %>.php'
-                ],
-                dest: 'build/'
-            }
-        },
+		// Clean the build folder.
+		clean: {
+			"pre-build": {
+				src: [
+					'build/',
+					'release/',
+					'vendor/'
+				]
+			}
+		},
 
-        compress: {
-            build: {
-                options: {
-                    archive: 'release/<%= pkg.name %>.zip'
-                },
-                expand: true,
-                files: [{
-                    expand: true,
-                    dest: '<%= pkg.name %>/',
-                    cwd: 'build/',
-                    src: ['**']
-                }]
-            }
-        }
+		// Copy to build folder.
+		copy: {
+			build: {
+				expand: true,
+				src: [
+					'partials/**',
+					'src/**',
+					'vendor/**',
+					'class-plugin.php',
+					'LICENSE',
+					'<%= pkg.name %>.php'
+				],
+				dest: 'build/'
+			}
+		},
 
-    });
+		compress: {
+			build: {
+				options: {
+					archive: 'release/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				dest: '<%= pkg.name %>/',
+				cwd: 'build/',
+				src: ['**']
+			}
+		}
 
-    require('load-grunt-tasks')(grunt);
-    grunt.registerTask('pre-build', ['clean:pre-build']);
-    grunt.registerTask('build', ['copy:build', 'compress:build']);
-    grunt.registerTask('pre-tag', ['version']);
+	} );
 
-    grunt.util.linefeed = '\n';
+	require( 'load-grunt-tasks' )( grunt );
+	grunt.registerTask( 'pre-build', ['clean:pre-build'] );
+	grunt.registerTask( 'build', ['copy:build', 'compress:build'] );
+	grunt.registerTask( 'pre-tag', ['version'] );
+
+	grunt.util.linefeed = '\n';
 
 };

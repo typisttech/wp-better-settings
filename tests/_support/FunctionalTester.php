05e8a3dd9ae88f35c673c3b0c56fe4b5d8dd88b8
Tang Rufus <tangrufus@gmail.com>
2017-04-11 10:06:53 +0800

    Apply code style

--- tests/_support/FunctionalTester.php
+++ tests/_support/FunctionalTester.php
@@ -14,7 +14,7 @@ namespace TypistTech\WPBetterSettings;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
  *
  * @SuppressWarnings(PHPMD)
  */

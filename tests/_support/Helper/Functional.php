c32047c970200598645fa1df85d066755c941fce
Tang Rufus <tangrufus@gmail.com>
2017-04-11 09:42:24 +0800

    vendor/bin/codecept generate:suite functional

---
+++ tests/_support/Helper/Functional.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TypistTech\WPBetterSettings\Helper;
+
+/**
+ * Here you can define custom actions
+ * All public methods declared in helper class will be available in $I
+ */
+class Functional extends \Codeception\Module
+{
+}

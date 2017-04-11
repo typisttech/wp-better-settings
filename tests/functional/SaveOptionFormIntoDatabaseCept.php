71e680f88af43caeaa9be93e6efd09ab7882b4e5
Tang Rufus <tangrufus@gmail.com>
2017-04-11 09:44:01 +0800

    Functional Test: Add SaveOptionFormIntoDatabaseCept
    ref: #74

---
+++ tests/functional/SaveOptionFormIntoDatabaseCept.php
@@ -0,0 +1,30 @@
+<?php use TypistTech\WPBetterSettings\FunctionalTester;
+
+$I = new FunctionalTester($scenario);
+$I->wantToTest('option form save options into database');
+
+$I->amOnWPBSOptionPage();
+
+$I->amGoingTo('submit option form');
+$formValues = [
+    'wpbs_option_1' => [
+        'my_name'     => 'Jane Doe',
+        'my_email'    => 'janedoe@example.com',
+        'my_url'      => 'https://www.example.com/janedoe',
+        'my_textarea' => 'I am jane doe.',
+        'my_checkbox' => true,
+    ],
+];
+$I->submitForm('form', $formValues);
+
+$I->wantToTest('options are saved into database and checkbox is transformed into 1');
+$I->seeOptionInDatabase([
+    'option_name'  => 'wpbs_option_1',
+    'option_value' => [
+        'my_name'     => 'Jane Doe',
+        'my_email'    => 'janedoe@example.com',
+        'my_url'      => 'https://www.example.com/janedoe',
+        'my_textarea' => 'I am jane doe.',
+        'my_checkbox' => '1',
+    ],
+]);

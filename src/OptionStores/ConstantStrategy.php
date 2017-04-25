6cd677cf048a7d6e907242043d95b8c2bc5ce1da
Tang Rufus <tangrufus@gmail.com>
2017-04-25 18:38:50 +0800

    Apply code style

--- src/OptionStores/ConstantStrategy.php
+++ src/OptionStores/ConstantStrategy.php
@@ -4,14 +4,14 @@
  *
  * A simplified OOP implementation of the WP Settings API.
  *
- * @package TypistTech\WPBetterSettings
+ * @package   TypistTech\WPBetterSettings
  *
- * @author Typist Tech <wp-better-settings@typist.tech>
+ * @author    Typist Tech <wp-better-settings@typist.tech>
  * @copyright 2017 Typist Tech
- * @license GPL-2.0+
+ * @license   GPL-2.0+
  *
- * @see https://www.typist.tech/projects/wp-better-settings
- * @see https://github.com/TypistTech/wp-better-settings
+ * @see       https://www.typist.tech/projects/wp-better-settings
+ * @see       https://github.com/TypistTech/wp-better-settings
  */
 
 declare(strict_types=1);

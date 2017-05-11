cad303e5c3620b298e301ebbefb29ff485d07110
Tang Rufus <tangrufus@gmail.com>
2017-05-11 17:39:57 +0800

    Show metaboxes on tabbed pages

--- src/partials/tabbed-page.php
+++ src/partials/tabbed-page.php
@@ -20,37 +20,61 @@ namespace TypistTech\WPBetterSettings;
 
 /* @var Decorators\Pages\TabbedPageInterface $context Context */
 
-echo '<div class="wrap">';
+?>
 
-do_action($context->getSnakecasedMenuSlug() . '_before_page_title');
+<div class="wrap">
 
-echo '<h1>' . esc_html($context->getPageTitle()) . '</h1>';
+    <?php do_action($context->getSnakecasedMenuSlug() . '_before_page_title'); ?>
 
-do_action($context->getSnakecasedMenuSlug() . '_after_page_title');
+    <h1><?php echo esc_html($context->getPageTitle()); ?></h1>
 
-do_action($context->getSnakecasedMenuSlug() . '_before_nav_tabs');
+    <?php do_action($context->getSnakecasedMenuSlug() . '_after_page_title'); ?>
 
-echo '<h2 class="nav-tab-wrapper">';
+    <?php do_action($context->getSnakecasedMenuSlug() . '_before_nav_tabs'); ?>
 
-/* @var Decorators\Page $tab */
-foreach ($context->getTabs() as $tab) {
-    $active = '';
-    if ($context->getMenuSlug() === $tab->getMenuSlug()) {
-        $active = ' nav-tab-active';
-    }
+    <h2 class="nav-tab-wrapper">
 
-    echo sprintf(
-        '<a href="%1$s" class="nav-tab%2$s" id="%3$s-tab">%4$s</a>',
-        esc_url($tab->getUrl()),
-        esc_attr($active),
-        esc_attr($tab->getMenuSlug()),
-        esc_html($tab->getMenuTitle())
-    );
-}
-echo '</h2>';
+        <?php
+        /* @var Decorators\Page $tab */
+        foreach ($context->getTabs() as $tab) {
+            $active = '';
+            if ($context->getMenuSlug() === $tab->getMenuSlug()) {
+                $active = ' nav-tab-active';
+            }
 
-do_action($context->getSnakecasedMenuSlug() . '_after_nav_tabs');
+            echo sprintf(
+                '<a href="%1$s" class="nav-tab%2$s" id="%3$s-tab">%4$s</a>',
+                esc_url($tab->getUrl()),
+                esc_attr($active),
+                esc_attr($tab->getMenuSlug()),
+                esc_html($tab->getMenuTitle())
+            );
+        }
+        ?>
 
-include __DIR__ . '/options-form.php';
+    </h2>
 
-echo '</div>';
+    <?php do_action($context->getSnakecasedMenuSlug() . '_after_nav_tabs'); ?>
+
+    <div id="poststuff">
+        <div id="post-body" class="metabox-holder columns-2">
+
+            <div id="post-body-content">
+                <?php include __DIR__ . '/options-form.php'; ?>
+            </div><!-- #post-body-content -->
+
+            <?php do_action($context->getSnakecasedMenuSlug() . '_before_postbox_containers'); ?>
+            <div id="postbox-container-1" class="postbox-container">
+                <?php do_meta_boxes($context->getSnakecasedMenuSlug(), 'side', null); ?>
+            </div>
+
+            <div id="postbox-container-2" class="postbox-container">
+                <?php do_meta_boxes($context->getSnakecasedMenuSlug(), 'normal', null); ?>
+                <?php do_meta_boxes($context->getSnakecasedMenuSlug(), 'advanced', null); ?>
+            </div>
+            <?php do_action($context->getSnakecasedMenuSlug() . '_after_postbox_containers'); ?>
+        </div><!-- #post-body -->
+
+    </div><!-- #poststuff -->
+
+</div><!-- .warp -->

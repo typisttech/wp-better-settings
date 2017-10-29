8ef6c454cc465ef8efb132fdcddc89145c06766a
Tang Rufus <tangrufus@gmail.com>
2017-10-29 17:37:28 +0800

    Test `Registrar::registerFields`

--- tests/wpunit/RegistrarTest.php
+++ tests/wpunit/RegistrarTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TypistTech\WPBetterSettings;
 
+use AdamWathan\Form\FormBuilder;
 use AspectMock\Test;
 use Codeception\TestCase\WPTestCase;
 
@@ -17,6 +18,9 @@ class RegistrarTest extends WPTestCase
         parent::setUp();
 
         $this->addSettingsSection = Test::func(__NAMESPACE__, 'add_settings_section', true);
+        $this->addSettingsField = Test::func(__NAMESPACE__, 'add_settings_field', true);
+
+        $this->builder = new FormBuilder();
 
         $this->subject = new Registrar('my-page');
     }
@@ -115,6 +119,74 @@ class RegistrarTest extends WPTestCase
     /** @test */
     public function it_register_fields_with_wordpress()
     {
-        $this->markTestIncomplete();
+        $section1 = new Section(
+            'my-section-1',
+            'My Section One'
+        );
+        $section1->add(
+            new Field(
+                'my_text',
+                'My Text',
+                $this->builder->text('my_text')
+                              ->addClass('regular-text')
+                              ->required()
+            ),
+            new Field(
+                'my_color',
+                'My Color',
+                $this->builder->select('my_color')
+                              ->addOption('red', 'Blood')
+                              ->addOption('blue', 'Sky')
+                              ->addOption('green', 'Grass')
+            )
+        );
+        $section2 = new Section(
+            'my-section-2',
+            'My Section Two'
+        );
+        $section2->add(
+            new Field(
+                'my_checkbox',
+                'My Checkbox',
+                $this->builder->checkbox('my_checkbox')
+            )
+        );
+        $this->subject->add($section1, $section2);
+
+        $this->subject->run();
+
+        $this->addSettingsField->verifyInvokedMultipleTimes(3);
+        $this->assertArraySubset(
+            [
+                [
+                    0 => 'my_text',
+                    1 => 'My Text',
+                    3 => 'my-page',
+                    4 => 'my-section-1',
+                    5 => [
+                        'label_for' => 'my_text',
+                    ],
+                ],
+                [
+                    0 => 'my_color',
+                    1 => 'My Color',
+                    3 => 'my-page',
+                    4 => 'my-section-1',
+                    5 => [
+                        'label_for' => 'my_color',
+                    ],
+                ],
+                [
+                    0 => 'my_checkbox',
+                    1 => 'My Checkbox',
+                    3 => 'my-page',
+                    4 => 'my-section-2',
+                    5 => [
+                        'label_for' => 'my_checkbox',
+                    ],
+                ],
+            ],
+            $this->addSettingsField->getCallsForMethod('add_settings_field')
+        );
     }
 }
